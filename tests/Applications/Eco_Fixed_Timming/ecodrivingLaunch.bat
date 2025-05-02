b14fe00e0ea17c0152ed260601b4a61f619a7b48
Hetian Guo <hg25079@uga.edu>
2025-05-01 22:56:57 -0400

    Add VISSIM Script; Not integrated with the FIXS yet.

--- tests/Applications/Eco_Fixed_Timming/ecodrivingLaunch.bat
+++
@@ -1,5 +0,0 @@
-
-:: start SUMO 
-start sumo-gui -c .\Test\chattCavMpr.sumocfg --remote-port 1337 --step-length 0.1 --netstate-dump .\Test\chattCavMpr.xml --netstate-dump.precision 10 --num-clients 2  --begin 28800 --end 33000
-:: start TrafficLayer
-start cmd /k .\TrafficLayer.exe -f .\ecodrivingConfig.yaml
