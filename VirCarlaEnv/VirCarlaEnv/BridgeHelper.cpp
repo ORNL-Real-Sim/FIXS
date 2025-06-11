0071c7e637328f1a4b8d4e829022db415ef1b95b
Hetian Guo <hg25079@uga.edu>
2025-06-10 22:10:42 -0400

    Add Helper Function for Sumo Carla TLS;

--- VirCarlaEnv/VirCarlaEnv/BridgeHelper.cpp
+++ VirCarlaEnv/VirCarlaEnv/BridgeHelper.cpp
@@ -1,6 +1,10 @@
 ﻿#include "BridgeHelper.h"
 
-carla::geom::Transform BridgeHelper::map_transfrom_sumo_to_carla(const carla::geom::Transform& in_sumo_transform,
+
+// Default value for offset
+carla::geom::Location BridgeHelper::offset = carla::geom::Location(0.0f, 0.0f, 0.0f);
+
+carla::geom::Transform BridgeHelper::map_transfrom_Sumo_to_Carla(const carla::geom::Transform& in_sumo_transform,
     const carla::geom::Vector3D& extent) {
     using namespace carla::geom;
 
@@ -46,33 +50,145 @@ carla::geom::Transform BridgeHelper::map_transfrom_Carla_to_Sumo(const carla::ge
     return Transform(out_location, out_rotation);
 }
 
-std::string BridgeHelper::map_sumo_vehicle_class_to_carla_typeId(const std::string& vclass)
+std::string BridgeHelper::map_Sumo_vClass_to_Carla_blueprintId(const std::string& vClass)
 {
-    static const std::unordered_map<std::string, std::string> mapVclassToTypeid = {
-        {"passenger",       "vehicle.tesla.model3"},
-        {"passenger/hov",   "vehicle.tesla.model3"},
-        {"bus",             "vehicle.mercedes.sprinter"},
-        {"coach",           "vehicle.carlamotors.carlacola"},
-        {"truck",           "vehicle.carlamotors.firetruck"},
-        {"delivery",        "vehicle.ford.ambulance"},
-        {"motorcycle",      "vehicle.kawasaki.ninja"},
-        {"bicycle",         "vehicle.bh.crossbike"},
-        {"pedestrian",      "walker.pedestrian.0001"},
-        {"emergency",       "vehicle.nissan.patrol"},
-        {"tram",            "static.prop.tram"},
+    static const std::unordered_set<std::string> carlaCarsBlueprints = {
+        "vehicle.audi.a2",
+		"vehicle.audi.etron",
+        "vehicle.audi.tt",
+		"vehicle.bmw.grandtourer",
+        "vehicle.chevrolet.impala",
+		"vehicle.citroen.c3",
+		"vehicle.dodge.charger_2020",
+		"vehicle.ford.mustang",
+		"vehicle.jeep.wrangler_rubicon",
+		"vehicle.lincoln.mkz_2017",
+		"vehicle.lincoln.mkz_2020",
+		"vehicle.mercedes.coupe",
+		"vehicle.mercedes.coupe_2020",
+		"vehicle.micro.microlino",
+        "vehicle.mini.cooper_s",
+        "vehicle.mini.cooper_s_2021",
+		"vehicle.nissan.micra",
+		"vehicle.nissan.patrol",
+		"vehicle.nissan.patrol_2021",
+		"vehicle.seat.leon",
+        "vehicle.tesla.model3",
+        "vehicle.toyota.prius",
+		"vehicle.ford.crown" // Also used for taxi
+	};
+    static const std::unordered_set<std::string> carlaTrucksBlueprints = {
+		"vehicle.carlamotors.carlacola",
+        "vehicle.carlamotors.european_hgv",
+        "vehicle.tesla.cybertruck",
     };
+    static const std::unordered_set<std::string> carlaVansBlueprints = {
+		"vehicle.mercedes.sprinter",
+        "vehicle.volkswagen.t2",
+		"vehicle.volkswagen.t2_2021",
+	};
+    static const std::unordered_set<std::string> carlaBusesBlueprints = {
+        "vehicle.mitsubishi.fusorosa",
+    };
+    static const std::unordered_set<std::string> carlaMotorcyclesBlueprints = {
+		"vehicle.harley-davidson.low_rider",
+        "vehicle.kawasaki.ninja",
+        "vehicle.vespa.zx125",
+        "vehicle.yamaha.yzf",
+    };
+    static const std::unordered_set<std::string> carlaBicyclesBlueprints = {
+        "vehicle.bh.crossbike",
+        "vehicle.diamondback.century",
+        "vehicle.gazelle.omafiets",
+	};
+    
+    static const std::unordered_set<std::string> carlaPedestriansBlueprints = {
+        "walker.pedestrian.0001",
+        "walker.pedestrian.0002",
+        "walker.pedestrian.0003",
+        "walker.pedestrian.0004",
+        "walker.pedestrian.0005",
+	};
+	//Note in the Carla Beprints, the emergency vehicles are not separated by type
+    static const std::unordered_set<std::string> carlaEmergencyBlueprints = {
+		"vehicle.ford.ambulance", // Vans
+        "vehicle.carlamotors.firetruck", // Truck
+		"vehicle.dodge.charger_police", // Car
+        "vehicle.dodge.charger_police_2020", // Car
+	};
 
-    auto it = mapVclassToTypeid.find(vclass);
-    if (it != mapVclassToTypeid.end()) {
-        return it->second;
+
+    std::string carlaBlueprintId = "";
+	//std::string carlaBlueprintId = "vehicle.tesla.model3"; // Default to be passenger car
+    if (vClass == "passenger"){
+		carlaBlueprintId = random_select_from_set(carlaCarsBlueprints);
+	}
+	else if (vClass == "truck") {
+        carlaBlueprintId = random_select_from_set(carlaTrucksBlueprints);
+    }
+    else if (vClass == "van") {
+        carlaBlueprintId = random_select_from_set(carlaVansBlueprints);
+    }
+    else if (vClass == "bus") {
+        carlaBlueprintId = random_select_from_set(carlaBusesBlueprints);
+    }
+    else if (vClass == "motorcycle") {
+        carlaBlueprintId = random_select_from_set(carlaMotorcyclesBlueprints);
+    }
+    else if (vClass == "bicycle") {
+        carlaBlueprintId = random_select_from_set(carlaBicyclesBlueprints);
+    }
+    else if (vClass == "pedestrian") {
+        carlaBlueprintId = random_select_from_set(carlaPedestriansBlueprints);
+    }
+    else if (vClass == "emergency") {
+        carlaBlueprintId = random_select_from_set(carlaEmergencyBlueprints);
     }
     else {
-        // Default fallback
-        return "vehicle.tesla.model3";
+		std::cerr << "Unknown vClass: " << vClass << std::endl
+		<< "Currently supported vClasses are:" << std::endl
+        << "passenger, truck, van, bus, motorcycle, bicycle, pedestrian, emergency." << std::endl
+		<< "Defaulting to vehicle.tesla.model3." << std::endl;
+        carlaBlueprintId = "vehicle.tesla.model3"; // Default to be passenger car
+    }
+	return carlaBlueprintId;
+}
+
+SumoTrafficLightState map_Carla_traffic_light_state_to_Sumo(carla::rpc::TrafficLightState carlaTrafficLightState) {
+    using namespace carla::rpc;
+    switch (carlaTrafficLightState) {
+    case TrafficLightState::Red:
+        return SumoTrafficLightState::RED;
+    case TrafficLightState::Yellow:
+        return SumoTrafficLightState::YELLOW;
+    case TrafficLightState::Green:
+        return SumoTrafficLightState::GREEN;
+    case TrafficLightState::Off:
+        return SumoTrafficLightState::OFF;
+    case TrafficLightState::Unknown:
+    default:
+        return SumoTrafficLightState::OFF;
     }
 }
 
+SumoTrafficLightState get_Sumo_traffic_light_state_from_char(char c) {
+    switch (c) {
+    case 'r': return SumoTrafficLightState::RED;
+    case 'y': return SumoTrafficLightState::YELLOW;
+    case 'G': return SumoTrafficLightState::GREEN;
+    case 'g': return SumoTrafficLightState::GREEN_WITHOUT_PRIORITY;
+    case 's': return SumoTrafficLightState::GREEN_RIGHT_TURN;
+    case 'u': return SumoTrafficLightState::RED_YELLOW;
+    case 'o': return SumoTrafficLightState::OFF_BLINKING;
+    case 'O': return SumoTrafficLightState::OFF;
+    default:
+        throw std::invalid_argument("Unknown SumoTrafficLightState char: " + std::string(1, c));
+    }
+}
 
+char Sumo_traffic_light_state_to_char(SumoTrafficLightState state) {
+    return static_cast<char>(state);
+}
 //tempDoublePtr = static_pointer_cast<libsumo::TraCIDouble> (VehDataSubscribeTraciResults[libsumo::VAR_ANGLE]);
 //CurVehData.heading = tempDoublePtr->value;
 
--- VirCarlaEnv/VirCarlaEnv/BridgeHelper.h
+++ VirCarlaEnv/VirCarlaEnv/BridgeHelper.h
@@ -4,11 +4,15 @@
 #include <string>
 #include <tuple>
 #include <cmath>
+#include <random>
+#include <stdexcept>
+#include "RandomUtils.h"
 #include <carla/geom/Transform.h>
 #include <carla/geom/Location.h>
 #include <carla/geom/Rotation.h>
 #include <carla/geom/Vector3D.h>
-
+#include <carla/client/TrafficLight.h>
+#include "carla/rpc/TrafficLightState.h"
 #define M_PI 3.14159265358979323846
 #define SET_CONTAINS_ID(set, value) ((set).find(value) != (set).end())
 
@@ -18,33 +22,55 @@ public:
     static carla::geom::Location offset;
 
     // Convert SUMO → Carla
-    static carla::geom::Transform map_transfrom_sumo_to_carla(const carla::geom::Transform& in_sumo_transform,
+    static carla::geom::Transform map_transfrom_Sumo_to_Carla(const carla::geom::Transform& in_sumo_transform,
         const carla::geom::Vector3D& extent);
 
     // Convert Carla → SUMO
     static carla::geom::Transform map_transfrom_Carla_to_Sumo(const carla::geom::Transform& in_carla_transform,
         const carla::geom::Vector3D& extent);
 
-    static std::string map_sumo_vehicle_class_to_carla_typeId(const std::string& vclass);
+    static std::string map_Sumo_vClass_to_Carla_blueprintId(const std::string& vclass);
 };
 
-// Default value for offset
-carla::geom::Location BridgeHelper::offset = carla::geom::Location(0.0f, 0.0f, 0.0f);
+
 
 struct Actor {
     std::string id;
-    std::string vclass;
+    std::string vClass;
     carla::geom::Transform transform;
     carla::geom::Vector3D extent;
+	// Additional properties can be added as needed, e.g.:
+	bool flagged = false; // Indicates if the actor is flagged for some reason
     //std::vector<std::string> signals;
     //carla::rpc::Color color;
 
+    Actor() = default;
+
     Actor(const std::string& _id,
-              const std::string& _vclass,
-              const carla::geom::Transform& _transform,
-              const carla::geom::Vector3D& _extent)
+        const std::string& _vClass,
+        const carla::geom::Transform& _transform,
+        const carla::geom::Vector3D& _extent)
         : id(_id),
-          vclass(_vclass),
-          transform(_transform),
-          extent(_extent) {}
+        vClass(_vClass),
+        transform(_transform),
+        extent(_extent) {
+    }
 };
+
+// Sumo signal state
+enum class SumoTrafficLightState : char {
+    RED = 'r',
+    YELLOW = 'y',
+    GREEN = 'G',
+    GREEN_WITHOUT_PRIORITY = 'g',
+    GREEN_RIGHT_TURN = 's',
+    RED_YELLOW = 'u',
+    OFF_BLINKING = 'o',
+    OFF = 'O'
+};
+
+char Sumo_traffic_light_state_to_char(SumoTrafficLightState state);
+
+SumoTrafficLightState get_Sumo_traffic_light_state_from_char(char c);
+
+SumoTrafficLightState map_Carla_traffic_light_state_to_Sumo(carla::rpc::TrafficLightState carlaTrafficLightState);
