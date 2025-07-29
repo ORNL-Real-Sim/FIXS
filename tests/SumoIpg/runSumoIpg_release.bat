7a6a433606f9a4b945ccb19dbc62f5a3486861c9
Yunli Shao <yunli.shao@uga.edu>
2025-07-29 00:09:57 -0400

    Revert "add comments and put signal sync under 0.1 cycle"
    This reverts commit d39526e7ce3599c8fc87ff3fc897c4b9958be309.

--- tests/SumoIpg/runSumoIpg_release.bat
+++ tests/SumoIpg/runSumoIpg_release.bat
@@ -1,11 +1,11 @@
 set RealSimPath=..\..\
 set RealSimAppPath=.\
 set configFilename=config_RS_noSimulinkRS.yaml
-set CmProjPath=C:\src_git\RS_FIXS_full\ProprietaryFiles\CM13_proj
+set CmProjPath=..\..\CM11_proj
 
 start sumo-gui -c %RealSimAppPath%\coordMerge.sumocfg --remote-port 1337 --step-length 0.1 --start
 
-start cmd /c C:\src_git\RS_FIXS_full\TrafficLayer\x64\Release\TrafficLayer.exe -f %configFilename%
+start cmd /c %RealSimPath%\TrafficLayer.exe -f %configFilename%
 
 call conda activate realsimdev
 python %RealSimPath%\CarMaker\RealSimSetCarMakerConfig.py --cm-project-path %CmProjPath% --configFile %configFilename%
