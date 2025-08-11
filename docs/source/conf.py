99f89a64e34900589616d7bf9cdfea6e8ded589e
Yunli Shao <yunli.shao@uga.edu>
2025-08-11 13:51:42 -0400

    sync previous #26 branch to resolve merge conflicts

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+import sphinx_rtd_theme
+
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Real-Sim'
+copyright = '2025, Sarayu Reddy'
+author = 'Sarayu Reddy'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
